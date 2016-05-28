7fcae9e9f08eb241600bc090e4563fe2578a72a5
mmoneib <mmoneib@gmx.com>
2016-05-29 00:54:53 +0200

    Initial commit.

---
+++ 4finance-HW/src/test/java/hello/TestHello.java
@@ -0,0 +1,27 @@
+package hello;
+
+import static org.junit.Assert.*;
+import io.fourfinanceit.homework.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebIntegrationTest
+public class TestHello {
+
+	private RestTemplate restTemplate = new TestRestTemplate();
+
+	@Test
+	public void test() {
+		System.out.println(restTemplate.getForEntity(
+				"http://localhost:8080/hello/", String.class));
+	}
+
+}
