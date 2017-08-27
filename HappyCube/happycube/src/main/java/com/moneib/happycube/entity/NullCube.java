5fca649467c4a547f322c19b4ff2c376613612ed
mmoneib <mmoneib@CAI1-L10724.vnet.valeo.com>
2017-08-27 16:48:50 +0200

    Adding Comments
    Comments added to NullCube and Nullified.

--- HappyCube/happycube/src/main/java/com/moneib/happycube/entity/NullCube.java
+++ HappyCube/happycube/src/main/java/com/moneib/happycube/entity/NullCube.java
@@ -2,26 +2,59 @@ package com.moneib.happycube.entity;
 
 import java.io.PrintWriter;
 
+import com.moneib.happycube.generator.AbstractCubeGenerator;
 import com.moneib.happycube.utility.Nullified;
 import com.moneib.happycube.utility.Printable;
 
+/**
+ * The Class NullCube is used to indicate an unsuccessful attempt to find a
+ * solution. It is propagated to the caller by the fall back strategy as defined
+ * in the {@link AbstractCubeGenerator generator}.
+ */
 public class NullCube extends Cube implements Printable, Nullified {
+
+	/**
+	 * The error message indicating the reason of the failure to find a
+	 * solution.
+	 */
 	private String error;
 
+	/**
+	 * Instantiates a new null cube. Here, the parameterless constructor of
+	 * {@link Cube} is called implicitly, which insures that the instantiated
+	 * null cube has no structure (faces).
+	 * 
+	 * @param error
+	 *            the error message indicating the reason of the failure to find
+	 *            a solution.
+	 */
 	public NullCube(String error) {
 		this.error = error;
 	}
 
+	/**
+	 * Prints the error message that led to the failure of finding a solution.
+	 */
 	@Override
 	public void print(PrintWriter writer) {
 		writer.println(error);
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.moneib.happycube.utility.Nullified#getError()
+	 */
 	@Override
 	public String getError() {
 		return error;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.moneib.happycube.utility.Nullified#setError(java.lang.String)
+	 */
 	@Override
 	public void setError(String error) {
 		this.error = error;
--- HappyCube/happycube/src/main/java/com/moneib/happycube/utility/Nullified.java
+++ HappyCube/happycube/src/main/java/com/moneib/happycube/utility/Nullified.java
@@ -1,7 +1,23 @@
 package com.moneib.happycube.utility;
 
+/**
+ * The Interface Nullified is used to indicate a null object and provide it with
+ * error propagation mechanism.
+ */
 public interface Nullified {
 
+	/**
+	 * Gets the error message that led to the failure of finding a solution.
+	 * 
+	 * @return the error message.
+	 */
 	public String getError();
+
+	/**
+	 * Sets the error message that led to the failure of finding a solution.
+	 * 
+	 * @param error
+	 *            the new error message.
+	 */
 	public void setError(String error);
 }
